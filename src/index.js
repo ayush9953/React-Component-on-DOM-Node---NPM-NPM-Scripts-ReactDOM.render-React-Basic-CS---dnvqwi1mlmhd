13955190bb7315e3c8b2e939e5fcac7e8edd9341
ayush9953 <77432436+ayush9953@users.noreply.github.com>
2022-09-07 17:20:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 
 
