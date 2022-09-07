8fbc922844b6e47a637818798fe7a7dd65dee70a
ayush9953 <77432436+ayush9953@users.noreply.github.com>
2022-09-07 17:38:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-function App(){
+export default function App(){
   return(
     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
   );
 }
-export default App;
+ 
 
